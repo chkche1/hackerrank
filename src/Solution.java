7347856ea32ff3802d51f2e7d6019cc6dffa40bf
Kent Chen <kuanchen@amazon.com>
2017-02-01 01:40:51 -0800

    Init commit with template and .gitignore

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Template
+ */
+public class Solution {
+    public static void main(String[] args) {
+        try(Scanner stdin = new Scanner(System.in)) {
+            // template
+        }
+    }
+}
