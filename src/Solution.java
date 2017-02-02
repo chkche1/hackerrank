3ced421a03439f97bff7f2cf7460d3fdbadd8125
Kent Chen <kuanchen@amazon.com>
2017-02-02 00:01:24 -0800

    Fixing styling in the template

--- src/Solution.java
+++ src/Solution.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 public class Solution {
     public static void main(String[] args) {
-        try(Scanner stdin = new Scanner(System.in)) {
+        try (Scanner stdin = new Scanner(System.in)) {
             // template
         }
     }
